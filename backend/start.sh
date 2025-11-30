a72e5c542dcacfbb97937a38a997449a778f6016
sebasmedina267 <sebasmedina267@gmail.com>
2025-11-30 17:27:58 +0100

    cambios para railway

--- backend/start.sh
+++ backend/start.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
+set -e
+
+# Migraciones y seeders
 php artisan migrate --force
 php artisan db:seed --force
+
+# Lanzar simulador en segundo plano
 php artisan simular:precios --loop --interval=5 &
+
+# Mantener Apache en foreground
 exec apache2-foreground
