48b8e8c350dc7a263027d4a23a5be0349676ac7d
sebasmedina267 <sebasmedina267@gmail.com>
2025-11-30 21:19:46 +0100

    Añadido cors

---
+++ backend/config/cors.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    'paths' => ['api/*'],
+
+    'allowed_methods' => ['*'],
+
+    'allowed_origins' => ['https://studio-3053804889-a5dfb.web.app'],
+
+    'allowed_headers' => ['*'],
+
+    'exposed_headers' => [],
+
+    'max_age' => 0,
+
+    'supports_credentials' => false,
+
+];
