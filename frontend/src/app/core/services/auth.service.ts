96da34347b4e6ca9692fa968bbf7e6ab5bfa90ea
sebasmedina267 <sebasmedina267@gmail.com>
2025-11-30 12:58:40 +0100

    pequeños cambios en servicios auth

--- frontend/src/app/core/services/auth.service.ts
+++ frontend/src/app/core/services/auth.service.ts
@@ -1,12 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, tap, BehaviorSubject } from 'rxjs';
+import { environment } from '../../../environments/environment.development';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  private baseUrl = 'http://localhost:8000/api';
+  private baseUrl = environment.apiUrl;
   private tokenKey = 'auth_token';
   private userKey = 'auth_user';
 
