0a946bb516665995ffcd0cefd5d40703fa4d1d08
sebasmedina267 <sebasmedina267@gmail.com>
2025-12-01 18:17:15 +0100

    cambios auth

--- backend/app/Http/Middleware/Authenticate.php
+++ backend/app/Http/Middleware/Authenticate.php
@@ -8,6 +8,6 @@ class Authenticate extends Middleware
 {
     protected function redirectTo($request): ?string
     {
-        return $request->expectsJson() ? null : route('login');
+        return null;
     }
 }
