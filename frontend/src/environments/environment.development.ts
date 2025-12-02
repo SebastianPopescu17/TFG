66c6bc39f6971d690ba5217270f3295127736031
sebasmedina267 <sebasmedina267@gmail.com>
2025-12-02 15:04:00 +0100

    Pequeños cambios

--- frontend/src/environments/environment.development.ts
+++ frontend/src/environments/environment.development.ts
@@ -1,6 +1,7 @@
 export const environment = {
- production: false,
- apiUrl: 'https://tfg-production-94d8.up.railway.app//api'
- // apiUrl: 'http://localhost:8000/api'
+ production: true,
+ // apiUrl: 'https://tfg-production-94d8.up.railway.app/api'
+ // apiUrl: 'https://fingrado-backend-production-e99b.up.railway.app/api'
+ apiUrl: 'http://localhost:8000/api'
   // apiUrl: 'http://127.0.0.1:8000/api'
 };
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,6 @@
 export const environment= {
   production: true,
-  apiUrl: 'https://tfg-production-94d8.up.railway.app/api'
+  // apiUrl: 'https://tfg-production-94d8.up.railway.app/api'
+  // apiUrl: 'https://fingrado-backend-production-e99b.up.railway.app/api'
+  apiUrl: 'http://localhost:8000/api'
 };
